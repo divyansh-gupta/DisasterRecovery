5b78f7217e71bccb4d6e13601154fc488f0ef1c7
Bhaanu Kaul <bhaanu.kaul1@gmail.com>
2017-04-18 13:10:32 -0400

    added more to gitignore

--- web/resources/DisasterRecoveryDB.sql
+++ web/resources/DisasterRecoveryDB.sql
@@ -53,8 +53,10 @@ CREATE TABLE Item (
 );
 
 -- Locations
+
 INSERT INTO Location (location_name, latitude, longitude, triggered) VALUES
 ('Burruss_Hall', 37.229, -80.42371, false), ('McBride_Hall', 37.23062, -80.42178, false), ('New_Classroom_Building', 37.229270, -80.427207, false);
+
 
 -- Responders
 INSERT INTO Responder (username, email, responder_name, image, password, location_id) VALUES
