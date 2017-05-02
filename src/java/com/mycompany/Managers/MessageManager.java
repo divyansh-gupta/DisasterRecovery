16745273775570690a64732a6a8eaa329200957a
divyansh-gupta <divyg@vt.edu>
2017-05-02 16:17:32 -0400

    Messages view shows something now

--- src/java/com/mycompany/Managers/MessageManager.java
+++ src/java/com/mycompany/Managers/MessageManager.java
@@ -15,8 +15,8 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Date;
 import java.util.HashMap;
+import java.util.Date;
 import java.util.Random;
 import java.util.stream.Collectors;
 import javax.ejb.EJB;
@@ -38,10 +38,28 @@ public class MessageManager implements Serializable {
     private Map<Location, List<Message>> locationMessagesMap;
     private Publisher publisher;
     private Subscriber subscriber;
+    private Location locationEngaged;
+
+    public String getMessageBeingTyped() {
+        return messageBeingTyped;
+    }
+
+    public void setMessageBeingTyped(String messageBeingTyped) {
+        this.messageBeingTyped = messageBeingTyped;
+    }
+    private String messageBeingTyped;
+
+    public Location getLocationEngaged() {
+        return locationEngaged;
+    }
+
+    public void setLocationEngaged(Location locationEngaged) {
+        this.locationEngaged = locationEngaged;
+    }
 
     @Inject
     private AccountManager accountManager;
-    
+
     @EJB
     private MessageFacade messageFacade;
 
@@ -57,26 +75,28 @@ public class MessageManager implements Serializable {
             System.exit(-1);
         }
     }
-    
-    public void sendMessage(String msgTxt, Location sendTo, Responder currentUser) {
+
+    public void sendMessage() {
+        Responder currentUser = accountManager.getSelected();
         Date date = new Date();
         Random rand = new Random(date.getTime());
         int randId = Math.abs(rand.nextInt());
         Message msg = new Message();
-        msg.setDescription(msgTxt);
+        msg.setDescription(this.messageBeingTyped);
         msg.setId(randId);
         msg.setSenderLocation(currentUser.getLocationId());
-        msg.setRecieverLocation(sendTo);
+        msg.setRecieverLocation(this.locationEngaged);
         msg.setTimeStamp(date);
         messageFacade.create(msg);
         try {
-            publisher.sendMessageToTopic(msgTxt, randId, currentUser.getLocationId().getId(), sendTo.getId(), date);
+            publisher.sendMessageToTopic(this.messageBeingTyped, randId, currentUser.getLocationId().getId(), locationEngaged.getId(), date);
         } catch (JMSException e) {
             System.err.println(e);
             System.exit(-1);
         }
+        this.messageBeingTyped = "";
     }
-    
+
     public Map<Location, List<Message>> getLocationMessagesMap() {
         int userLocId = accountManager.getSelected().getLocationId().getId();
         locationMessagesMap = messageFacade.findAll().stream().distinct()
@@ -85,9 +105,22 @@ public class MessageManager implements Serializable {
         System.out.println(locationMessagesMap);
         return locationMessagesMap;
     }
-    
+
+    public List<Message> messagesBetweenSelectedLocationAndUser() {
+        if (this.locationEngaged == null) {
+            return new ArrayList();
+        }
+        List<Message> compiled = getLocationMessagesMap().get(this.locationEngaged);
+        compiled.addAll(locationMessagesMap.get(accountManager.getSelected().getLocationId()));
+        return compiled;
+    }
+
     public List<Location> getMessageLocations() {
-        return new ArrayList(getLocationMessagesMap().keySet());
+        List<Location> locations = new ArrayList(getLocationMessagesMap().keySet());
+        if (locations.size() > 0) {
+            this.locationEngaged = locations.get(0);
+        }
+        return locations;
     }
 
     public void setLocationMessagesMap(Map<Location, List<Message>> locationMessagesMap) {
